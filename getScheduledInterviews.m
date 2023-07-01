7d4f8c88a2989635b2c9137b647eb14bdc859277
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 20:44:19 +0530

    Add files via upload

--- getScheduledInterviews.m
+++ getScheduledInterviews.m
@@ -1,7 +1,7 @@
 
 function data = getScheduledInterviews(iName) 
     conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
-    query = "SELECT ScheduledInterviews.StartingTime, ScheduledInterviews.EndingTime, Candidates.Name, Candidates.Domain, Candidates.Department, Candidates.Round1, Candidates.Round2, Candidates.Round3 FROM " ...
+    query = "SELECT ScheduledInterviews.StartingTime, ScheduledInterviews.EndingTime, Candidates.Name, Candidates.ID, Candidates.Domain, Candidates.Department, Candidates.Round1, Candidates.Round2, Candidates.Round3 FROM " ...
         + "Candidates JOIN ScheduledInterviews ON Candidates.ID = ScheduledInterviews.StudentID WHERE ScheduledInterviews.InterviewerName = " + '"' + iName+ '"' + ';';
     data = fetch(conn,query);
     
