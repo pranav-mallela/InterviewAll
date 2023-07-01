ec934600e0c1b4d55c9b35a4d9d2e606e979b01a
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:37:17 +0530

    added candidate detail api

---
+++ getCandidateDetail.m
@@ -0,0 +1,10 @@
+
+function data = getCandidateDetail(name)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+
+    query = 'SELECT * FROM Candidates WHERE Name = ' + name + ';';
+    
+    data = fetch(conn,query);
+    close(conn)
+    clear conn query
+end 
