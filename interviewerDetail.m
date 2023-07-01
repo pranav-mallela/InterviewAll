ff8a2a4052b712f19ca90b7829c371debf575ed6
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:38:43 +0530

    added interviewer detail API

---
+++ interviewerDetail.m
@@ -0,0 +1,11 @@
+
+function data = interviewerDetail (name, dept)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+    
+    
+    query = 'SELECT * FROM Interviewer'+dept+'WHERE Name = ' + name + ';';
+    
+    data = fetch(conn,query);
+    close(conn)
+    clear conn query
+end 
