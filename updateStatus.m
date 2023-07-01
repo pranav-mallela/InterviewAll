d242da3a04f727d73be34f80b0cc796249ac5b6c
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:48:04 +0530

    added review functionality

---
+++ updateStatus.m
@@ -0,0 +1,11 @@
+
+function data = updateStatus (name,status, dept)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+    
+    
+    query = 'UPDATE Interviewer'+dept+'SET Status = ' + status +'WHERE Name = ' + name + ';';
+    
+    data = fetch(conn,query);
+    close(conn)
+    clear conn query
+end 
