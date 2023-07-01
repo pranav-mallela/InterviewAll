552af4ec0e7e64e7814340b833ca0bd6976fffa5
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:38:18 +0530

    Scheduled interviews API

---
+++ getScheduledInterviews.m
@@ -0,0 +1,11 @@
+
+function data = getScheduledInterviews(iName) 
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+    query = ['SELECT ScheduledInterviews.StartingTime, ScheduledInterviews.EndingTime, Candidates.Name, Candidates.Domain, Candidates.Department, ScheduledInterviews.Round1, ScheduledInterviews.Round2, ScheduledInterviews.Round3 FROM ' ...
+        'Candidates JOIN ScheduledInterviews ON Candidates.ID = ScheduledInterviews.StudentID WHERE ScheduledInterviews.InterviewerName = ' + iName+ ';'];
+    data = fetch(conn,query);
+    
+    close(conn)
+    
+    clear conn query
+end
