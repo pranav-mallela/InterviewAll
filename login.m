5f52b508bf2fc71551eed747e89c7d4b34ede7e6
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 18:08:18 +0530

    Added login functionality

---
+++ login.m
@@ -0,0 +1,38 @@
+function n = login (name,email)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+    
+    n = -1;
+
+    query = 'SELECT * FROM Admin Where Name = ' + name + ' AND Email = ' + email;
+    data = fetch(conn,query);
+    if(isempty(data)) 
+
+        query = 'SELECT * FROM InterviewerTECH Where Name = ' + name + 'AND Email = ' + email;
+        data = fetch(conn,query);
+        if(isempty(data))
+            
+            query = 'SELECT * FROM InterviewerHR Where Name = ' + name + 'AND Email = ' + email;
+            data = fetch(conn,query);
+
+            if(isempty(data)) 
+                   query = 'SELECT * FROM InterviewerMG Where Name = ' + name + 'AND Email = ' + email;
+                   data = fetch(conn,query);
+                   if(isempty(data))
+                       n = -1
+                   else 
+                       n = 0
+                   end
+           
+            else 
+                n = 0
+            end 
+        else
+            n = 0
+        end 
+    else
+        n = 1
+    end 
+     
+    close(conn)
+    clear conn query
+end 
