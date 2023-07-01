33cdf22ebe1aea4dc2ed92fb9d2dbcb0c960f8f5
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:39:08 +0530

    added evaluation update API

---
+++ updateEvaluation.m
@@ -0,0 +1,10 @@
+
+function data = updateEvauation(name,status)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+
+    query = 'UPDATE Candidates SET Status = ' + status +'WHERE Name = ' + name + ';';
+    
+    data = fetch(conn,query);
+    close(conn)
+    clear conn query
+end 
