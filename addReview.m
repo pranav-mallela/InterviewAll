9200f1ab2c27b5a68c38b6e91f4e655d249e2db7
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:36:46 +0530

    Add files via upload

---
+++ addReview.m
@@ -0,0 +1,10 @@
+
+function data = addReview (id, review, status)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+
+    query = 'UPDATE Reviews SET Status = ' + status +'Review =' + review + 'WHERE StudentID = ' + id + ';';
+    
+    data = fetch(conn,query);
+    close(conn)
+    clear conn query
+end 
