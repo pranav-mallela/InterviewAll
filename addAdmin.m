f971f2e3f792e1155180a4ddad3fb572fbe5bd18
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 16:47:15 +0530

    added add Admin functionality

---
+++ addAdmin.m
@@ -0,0 +1,11 @@
+
+function data = addAdmin (name,email)
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+    
+    
+    query = 'INSERT INTO admin (name, email) VALUES (' +  name + ', ' +  email + ');';
+    
+    data = fetch(conn,query);
+    close(conn)
+    clear conn query
+end 
