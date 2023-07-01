bfb2b17be9cc210e1897d2e07441353756500ad9
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 19:32:26 +0530

    bug fix

--- getOngoingInterviews.m
+++ getOngoingInterviews.m
@@ -1,11 +1,8 @@
 
 function data = getOngoingInterviews(startTime,endTime)
     conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
-    
-    string t1 =  num2str(startTime);
-    string t2 =  num2str(endTime);
-
-    query = ["SELECT * FROM ScheduledInterviews WHERE start_time >= " + t1 + " AND end_time <= "+ t2 + ';'];
+   
+    query = "SELECT * FROM ScheduledInterviews WHERE StartingTime >= " + '"' + startTime + '"' + " AND EndingTime <= "+ '"' + endTime + '"' + ';';
     
     data = fetch(conn,query);
     close(conn)
