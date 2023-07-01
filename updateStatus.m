441cdc7c4c8026bcc1cd1d45a095b742e57a733e
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-01 21:14:20 +0530

    Add files via upload

--- updateStatus.m
+++ updateStatus.m
@@ -1,8 +1,23 @@
 
-function updateStatus (name,status, dept)
+function updateStatus (name,status)
     conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
     
-    
+    query = "SELECT * FROM InterviewerHR WHERE Name = " + '"' + name + '"' + ';';
+    data = fetch(conn,query);
+
+    dept = "HR";
+
+    if(isempty(data))
+         query = "SELECT * FROM InterviewerTECH WHERE Name = " + '"' + name + '"' + ';';
+         data = fetch(conn,query);
+         dept = "TECH";
+
+         if(isempty(data))
+            dept = "MG";
+         end 
+    end
+
+
     query = "UPDATE Interviewer"+dept+" SET Status = " + '"' + status + '"' +" WHERE Name = " + '"' + name + '"' +';';
     
     execute(conn,query);
