c8b134dad133dca67de1f14f4d30341e528a2dfe
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-02 20:14:24 +0530

    added error catch

--- signup.m
+++ signup.m
@@ -1,15 +1,19 @@
-function signup (name, department, domain, mailid, password)
+function err = signup (name, department, domain, mailid, password)
     conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
-    
+    err = 0;
     hashedPassword = DataHash(password);
 
     defaultStatus = "Idle";
-    query = "INSERT INTO Interviewer" + department + " (Name, Department, Domain, Email, Status) VALUES (" +  '"'+name+'"' + ', ' + '"'+ department+'"' + ', ' + '"'+ domain+'"' + ', ' + '"'+ mailid+'"'+ ', ' + '"'+ defaultStatus+'"' + ");";
-    exec(conn,query);
-
-    query = "INSERT INTO Credentials VALUES (" + '"' + name + '"' + ',' + '"' + hashedPassword + '"' + ';';
-    exec(conn,query)
+    try
+        query = "INSERT INTO Interviewer" + department + " (Name, Department, Domain, Email, Status) VALUES (" +  '"'+name+'"' + ', ' + '"'+ department+'"' + ', ' + '"'+ domain+'"' + ', ' + '"'+ mailid+'"'+ ', ' + '"'+ defaultStatus+'"' + ");";
+        exec(conn,query);
     
+        query = "INSERT INTO Credentials VALUES (" + '"' + name + '"' + ',' + '"' + hashedPassword + '"' + ';';
+        exec(conn,query)
+    catch
+        err = 1;
+    end
+
     close(conn)
     clear conn query
 end 
