ae7b6ce5b3754b8e5a6327c0d661a48fda0a3d53
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-02 21:30:21 +0530

    added password functionality

--- addAdmin.m
+++ addAdmin.m
@@ -1,14 +1,18 @@
 
-function err = addAdmin (name,email)
+function err = addAdmin (name,email, password)
     err = 0;
+    hashedPassword = dataHash(password);
 
     try 
         conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
         
         
         query = "INSERT INTO admin (name, email) VALUES (" +  '"'+name+'"' + ', ' + '"'+ email+'"' + ");";
-        
         exec(conn,query);
+
+        query = "INSERT INTO Credentials VALUES (" + '"' + name + '"' + ',' + '"' + hashedPassword + '")' + ';';
+        exec(conn,query)
+
     catch 
         err = 1;
     end 
