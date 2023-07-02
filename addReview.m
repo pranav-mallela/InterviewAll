adb4b1fcc693559ce045e8033d1d5bc9b0a1f489
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-02 11:33:56 +0530

    query and bug fix

--- addReview.m
+++ addReview.m
@@ -2,13 +2,13 @@
 function addReview (id, iName, review, status)
     conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
     
-        query = "SELECT * FROM InterviewerHR WHERE Name = " + '"' + name + '"' + ';';
+    query = "SELECT * FROM InterviewerHR WHERE Name = " + '"' + iName + '"' + ';';
     data = fetch(conn,query);
 
     round = "Round3";
 
     if(isempty(data))
-         query = "SELECT * FROM InterviewerTECH WHERE Name = " + '"' + name + '"' + ';';
+         query = "SELECT * FROM InterviewerTECH WHERE Name = " + '"' + iName + '"' + ';';
          data = fetch(conn,query);
          round = "Round1";
 
@@ -17,7 +17,7 @@ function addReview (id, iName, review, status)
          end 
     end
 
-    query = "INSERT INTO Reviews (StudentID, Round,InterviewerName, Review, Status) VALUES (" +  '"'+id+'"' + ', ' + '"'+ round+'"' + ', ' + '"'+ iName+'"' + ', ' + '"'+ review+'"' + '"'+ status+'"' + ");";
+    query = "INSERT INTO Reviews (StudentID, Round,InterviewerName, Review, Status) VALUES (" +  '"'+id+'"' + ', ' + '"'+ round+'"' + ', ' + '"'+ iName+'"' + ', ' + '"'+ review+'"' + ', '+ '"'+ status+'"' + ");";
     
     exec(conn,query);
     close(conn)
