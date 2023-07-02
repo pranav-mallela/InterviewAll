e647da30e5fa4f661050969c0100c9c7c9aed1b5
ShisuiMadara <77777434+ShisuiMadara@users.noreply.github.com>
2023-07-02 22:27:42 +0530

    bug fix

--- getFreeInterviewers.m
+++ getFreeInterviewers.m
@@ -1,99 +1,191 @@
 function err = getFreeInterviewers(name, date) %yyyy-mm-dd
+
     h = actxserver('Outlook.Application');
+
     namespace = h.GetNamespace('MAPI');
-    calendars = namespace.GetDefaultFolder(9); 
+
+    calendars = namespace.GetDefaultFolder(9);
+
    
+
     items = calendars.Items;
+
     data = [];
+
     date = convertStringsToChars(date);
+
     filteredItems = items.Restrict(['[Start] >= ''' date ' 00:00 AM''' ' AND [End] <= ''' date ' 11:59 PM''']);
 
+    conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
+
+    err = 0;
+
     try
-        conn = sqlite('\\mathworks\devel\sandbox\gagarwal\database\InterviewScheduler.db');
-        
-    
+
         query = "SELECT * FROM InterviewerHR WHERE Name = " + '"' + name + '"' + ';';
+
         data = fetch(conn,query);
+
         dept = "HR";
-    catch 
+
+    catch
+
         err = 1;
-    end 
+
+    end
+
+ 
 
    
+
     if(isempty(data))
+
         try
+
              query = "SELECT * FROM InterviewerTECH WHERE Name = " + '"' + name + '"' + ';';
+
              data = fetch(conn,query);
+
              dept = "TECH";
-        catch 
+
+        catch
+
             err = 1;
 
-        end 
+ 
+
+        end
+
    
 
+ 
+
          if(isempty(data))
+
             dept = "MG";
 
-         end 
+ 
+
+         end
+
     end
+
+ 
 
     int_startTime = 900;
 
+ 
+
     for i = 1 : filteredItems.Count
+
         meeting = filteredItems.Item(i);
+
         time = (meeting.Start);
+
         temp = split(time, " ");
+
         temp = split(temp(2), ":");
+
         he = "";
+
         he = he + temp(1);
+
         he = he + temp(2);
+
         
+
         startTime = he;
+
            
+
         time = (meeting.End);
+
         temp = split(time, " ");
+
         temp = split(temp(2), ":");
+
         he = "";
+
         he = he + temp(1);
+
         he = he + temp(2);
+
+ 
 
         endTime = he;
 
+ 
+
         freeSlots = [];
+
         startTime = str2double(startTime);
+
         endTime = str2double(endTime);
+
         if(int_startTime < startTime)
+
             if(endTime <= 1800)
+
                 temp = endTime;
-                while(temp - int_startTime >= 100) 
+
+                while(temp - int_startTime >= 100)
+
                     
-                    freeSlots = [freeSlots, [int_startTime, int_startTime + 100]]
+
+                    freeSlots = [freeSlots, [int_startTime, int_startTime + 100]];
+
                     int_startTime = int_startTime + 100;
-                end 
+
+                end
+
             end
+
         end
+
                 
-        disp(size(freeSlots))
+
         for j = 1 : 2 : size(freeSlots)
+
             st  = freeSlots(j);
+
             en = freeSlots(j+1);
 
-                try 
-                    query = "INSERT INTO FreeInterviewer" + dept + " VALUES (" +  '"'+name+'"' + ', ' + '"'+ st+'"' + ', ' + '"'+ en+'"' + ', ' + '"'+ dept+'"' + ");";
-                    
+ 
+
+                try
+
+                    query = "INSERT INTO FreeInterviewer" + dept + " VALUES (" +  '"'+name+'"' + ', ' + '"'+ st+'"' + ', ' + '"'+ en+'"' + ', ' + '"'+ dept+'"' +  ', ' + '"'+ "Yes" +'"' + ");";
+
+                    display(query)
+
                     exec(conn, query)
-                catch 
+
+                catch
+
                         err = 1;
-                end 
+
+                end
+
         end
+
+ 
 
         int_startTime = endTime;
 
+ 
+
     end
+
     
+
     scheduler();
+
     
+
     close(conn)
+
     clear conn query
-    h.release; 
+
+    h.release;
+
 end
