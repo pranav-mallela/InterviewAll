a7049458eb0eff457576cb32c994c902f251893e
HARSHAL RITWIK <91392069+Ritwik1010@users.noreply.github.com>
2023-07-03 13:49:15 +0530

    Add files via upload

--- getPassedCandidates.m
+++ getPassedCandidates.m
@@ -6,7 +6,7 @@ function [err, data] = getPassedCandidates()
     
         query = "SELECT Name, ID, Status, Round1, Round2, Round3, Domain, Email FROM Candidates " + ...
             "WHERE Round1<>" + '"Ongoing" ' + "AND Round2<>" + '"Ongoing" '+ "AND Round3<>" + '"Ongoing" '+ "AND Round1<>" + '"Not Completed" ' + "AND Round2<>" + '"Not Completed" '+ "AND Round3<>" + '"Not Completed" ' + ';';
-        disp(query)
+       % disp(query)
         data = fetch(conn,query);
     catch
         err = 1;
